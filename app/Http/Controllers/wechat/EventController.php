85ce950e32969b4d35239ecabbe956f0c3185dc4
jmm888 <750726766@@qq.com>
2019-09-28 09:19:16 +0800

    aa

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -123,7 +123,7 @@ class EventController extends Controller
                 echo $xml_str;
             }
         }*/
-        dd($xml_arr);
+        //dd($xml_arr);
         //关注逻辑
         if($xml_arr['MsgType']=='event' && $xml_arr['Event']=='subscribe'){
             //关注
