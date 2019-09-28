fbd536347427e1cc3cd2a395ce0309be2badc62c
jmm888 <750726766@@qq.com>
2019-09-28 10:43:08 +0800

    aa

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -24,10 +24,10 @@ class EventController extends Controller
 
         $xml_string = file_get_contents('php://input');//是个可以访问请求的原始数据的只读流
 //        dd($xml_string);
-        $wechat_log_path=storage_path('logs/wechat/').date('Y-m-d').'.log';
-        file_put_contents($wechat_log_path,"--------------------------\n",FILE_APPEND);
-        file_put_contents($wechat_log_path,$xml_string,FILE_APPEND);
-        file_put_contents($wechat_log_path,"\n--------------------------\n\n",FILE_APPEND);
+        //$wechat_log_path=storage_path('logs/wechat/').date('Y-m-d').'.log';
+        //file_put_contents($wechat_log_path,"--------------------------\n",FILE_APPEND);
+        //ile_put_contents($wechat_log_path,$xml_string,FILE_APPEND);
+        //file_put_contents($wechat_log_path,"\n--------------------------\n\n",FILE_APPEND);
         $xml_obj=simplexml_load_string($xml_string,'SimpleXMLElement',LIBXML_NOCDATA);
 //        dd($xml_obj);
         $xml_arr=(array)$xml_obj;
